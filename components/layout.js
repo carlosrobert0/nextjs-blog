69cfa847e132848e60d59bcbb219a9f024f37271
Carlos Roberto <crneto@stefanini.com>
2021-07-01 21:46:00 -0300

    Add ajusts

--- components/layout.js
+++ components/layout.js
@@ -4,8 +4,8 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = 'Carlos Devs'
-export const siteTitle = 'Next.js Sample Website'
+const name = 'Carlos Dev'
+export const siteTitle = 'Next.js Sample Website | Carlos Dev'
 
 export default function Layout({ children, home }) {
   return (
